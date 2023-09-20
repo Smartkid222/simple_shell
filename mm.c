14e8c7a3b15c20e5f071b1afd37360832f72c906
Smartkid222 <emekam848@gmail.com>
2023-09-20 14:19:37 -0700

    shell

--- mm.c
+++ mm.c
@@ -1,108 +1,18 @@
 #include "shell.h"
 
 /**
- **_realloc -  Reallocates A Memory Block Using Malloc And Free
- *@ptr: Pointer
- *@old_size: Previous Size Of The Pointer
- *@new_size: New Size Of The Pointer
- *Return: Void Pointer Rellocated Memory
+ * bfree - frees a pointer and NULLs the address
+ * @ptr: address of the pointer to free
+ *
+ * Return: 1 if freed, otherwise 0.
  */
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+int bfree(void **ptr)
 {
-	void *result;
-
-	if (new_size == old_size)
-		return (ptr);
-	if (new_size == 0 && ptr)
+	if (ptr && *ptr)
 	{
-		free(ptr);
-		return (NULL);
+		free(*ptr);
+		*ptr = NULL;
+		return (1);
 	}
-	result = malloc(new_size);
-	if (result == NULL)
-		return (NULL);
-	if (ptr == NULL)
-	{
-		fill_an_array(result, '\0', new_size);
-		free(ptr);
-	}
-	else
-	{
-		_memcpy(result, ptr, old_size);
-		free(ptr);
-	}
-	return (result);
-
-}
-/**
- * free_all - Free Array Of Char Pointer And Char Pointer
- * @cmd:Array Pointer
- * @line:Char Pointer
- * Return: Void
- */
-void free_all(char **cmd, char *line)
-{
-	free(cmd);
-	free(line);
-	cmd = NULL;
-	line = NULL;
-}
-
-/**
- * _memcpy - Copy Byte From Source To Destination
- * @dest: Destination Pointer
- * @src: Source Pointer
- * @n: Size (How Much You Will Copy)
- *Return: Void Pointer
- */
-char *_memcpy(char *dest, char *src, unsigned int n)
-{
-	unsigned int i;
-
-	for (i = 0; i < n; i++)
-	{
-		dest[i] = src[i];
-	}
-	return (dest);
-}
-/**
- * fill_an_array - Fill An Array By Constant Byte
- * @a:Void Pointer
- * @el: Int
- * @len:Length Int
- *Return: Void Pointer
- */
-void *fill_an_array(void *a, int el, unsigned int len)
-{
-	char *p = a;
-	unsigned int i = 0;
-
-	while (i < len)
-	{
-		*p = el;
-		p++;
-		i++;
-	}
-	return (a);
-}
-/**
- * _calloc -  Allocates Memory For An Array, Using Malloc.
- * @size: Size
- * Return: Void Pointer
- */
-void *_calloc(unsigned int size)
-{
-	char *a;
-	unsigned int i;
-
-	if (size == 0)
-	return (NULL);
-	a = malloc(size);
-	if (a == NULL)
-	return (NULL);
-	for (i = 0; i < size; i++)
-	{
-		a[i] = '\0';
-	}
-	return (a);
+	return (0);
 }
